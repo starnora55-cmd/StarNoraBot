99b58f83a5a1a8f44dc9fb25de95f624b490905f
starnora55-cmd <starnora55@gmail.com>
2026-03-29 12:08:02 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -13,7 +13,7 @@ module.exports = {
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
     newsletterJid: '120363161513685998@newsletter', // Newsletter JID for menu forwarding
-    updateZipUrl: 'https://github.com/cr/StarNoraBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
+    updateZipUrl: 'https://github.com/starnora55-cmd/StarNoraBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
     packname: 'StarNoraBot Mini',
